f35f1a6b026fe50086f675dd130d22e7c54528b2
Schaslivchik <schaslivchikm@gmail.com>
2022-02-05 22:32:39 +0200

    create user

--- js/index.js
+++ js/index.js
@@ -1,2 +1,12 @@
 'use strict'
 console.log('tralalla');
+
+class User{
+    constructor(name,surname,age){
+        this.firstName = name;
+        this.lastName = surname;
+        this.age = age;
+    }
+}
+
+const user = new User('Mikkhai', 'Shapovalov', 23)
