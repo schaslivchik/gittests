8b04a9d25800952b55b95dbc3e8ec97ed5ff3d88
Schaslivchik <schaslivchikm@gmail.com>
2022-02-05 22:48:04 +0200

    u

--- js/index.js
+++ js/index.js
@@ -7,6 +7,19 @@ class User{
         this.lastName = surname;
         this.age = age;
     }
+    get fullName(){
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+const user = new User('Mikkhai', 'Shapovalov', 23)
+
+class UserInfo extends User {
+    constructor(name, surname, age, isBanned, isSubscribed){
+        super(name, surname, age);
+        this.isBanned = isBanned;
+        this.isSubscribed = isSubscribed; 
+    }
+    
 }
 
-const user = new User('Mikkhai', 'Shapovalov', 23)
+const user1 = new UserInfo('Nikhai', 'Busarov', 23, false, false)
